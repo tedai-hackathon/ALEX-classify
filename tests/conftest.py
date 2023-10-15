06d454836d361da5fd56aa66eaf2a775631621f7
Yassin Kortam <yassinkortam@g.ucla.edu>
2023-10-15 05:52:38 -0700

    fix: fixed tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,8 @@ def entities_json_path():
 @pytest.fixture(scope="session")
 def flags_json_path():
     return "/Users/yassinkortam/Documents/GitHub/ALEX-classify/flags.json"
+
+
+@pytest.fixture(scope="session")
+def docs_dir():
+    return "docs"
--- tests/test_classify_entity.py
+++ tests/test_classify_entity.py
@@ -4,7 +4,7 @@ from classify_entity import ClassifyEntity
 class TestClassifyEntity:
     """ """
 
-    def test_alex_classify(self, entities_json_path, flags_json_path):
+    def test_alex_classify(self, entities_json_path, flags_json_path, docs_dir):
         """ """
-        classify = ClassifyEntity(entities_json_path, flags_json_path)
+        classify = ClassifyEntity(entities_json_path, flags_json_path, docs_dir)
         assert classify is not None
